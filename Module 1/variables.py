4151554e7de6bccd80d62544363ead3879c913fa
florjon <floni.flori@gmail.com>
2025-11-08 14:33:22 +0100

    lesson1 dona shume ubo e shtir

---
+++ Module 1/variables.py
@@ -0,0 +1,24 @@
+name = "Florjon"
+print(name)
+
+temperature = 98.6
+print(temperature)
+print(type(temperature))
+
+
+
+age= 30
+print("Age is:",age)
+
+x = 5
+y = 3
+result = x + y
+
+print("Rezultati eshte :" ,result)
+
+age = + 1
+print("age now is:", age)
+
+first_name = "john"
+last_name = "doe"
+full_name = first_name + " " + last_name
