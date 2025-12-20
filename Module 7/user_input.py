a89a1ec13e209296b9cedc2ba0002b8094a0dd3a
Florjon <floni.flori@gmail.com>
2025-12-20 14:37:34 +0100

    shum atraktive ora

---
+++ Module 7/user_input.py
@@ -0,0 +1,15 @@
+
+
+name = input("Enter your name:")
+print(f"Hello,{name}")
+
+age = int(input("Enter your age:"))
+print(type(age))
+
+
+number1 = int(input("Enter first number:"))
+number2 = int(input("Enter second number:"))
+
+result = number1 + number2
+
+print(f"The sum of {number1} and {number2} is {result}")
